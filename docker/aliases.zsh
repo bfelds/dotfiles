e8a00f617b041339ff7c03bef353013f3e947a0c
Ben Feldman <ben@goharness.com>
2025-01-17 09:51:43 -0500

    latest

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,2 +1,4 @@
 alias d='docker $*'
-alias d-c='docker-compose $*'
+alias d-c='docker compose $*'
+alias dcu='docker compose up '
+alias dcd='docker compose down'
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,10 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+
+
+alias ll='ls -altr'
+alias harness='cd ~/code/harness'
+alias aws_stage='export AWS_PROFILE=Infrastructure-478688809168'
+alias aws_prod='export AWS_PROFILE=Infrastructure-345273255759'
+alias aws_login='aws sso login'
